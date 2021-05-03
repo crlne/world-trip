24eebfa1535b63675174b48e41d1b9dcdb2a0a3b
crlne <carol.lages@outlook.com>
2021-05-02 21:57:50 -0300

    text home page

--- src/components/Destination.tsx
+++ src/components/Destination.tsx
@@ -1,12 +1,22 @@
-import { Center, Flex, WrapItem } from "@chakra-ui/react";
+import { Center, Flex, Text, WrapItem } from "@chakra-ui/react";
 
 export default function Destination() {
     return (
-        <Flex maxWidth={1140}>
-            <WrapItem mt="190" ml="605">
-            <Center w="90px" h="0" bg="gray.500" border="2px" ml="20%">   
+        <Flex maxWidth={1140} align="center" m="auto" justify="center" direction="row">
+            <WrapItem mt="90" position="absolute"> 
+            <Center w="90px" h="0" bg="gray.500" opacity="0.5" border="2px" ml="20%">   
             </Center>
         </WrapItem>
+
+        <Flex mt="252">
+            <Text fontSize="2rem" align="center" fontWeight="500" lineHeight="54px">
+                Vamos nessa?
+                <Text>
+                    Então escolha o continente
+                </Text> 
+            </Text>
+
+        </Flex>
         </Flex>
     )
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,23 @@
-import { Image, Flex } from "@chakra-ui/react";
+import { Image, Flex, Heading } from "@chakra-ui/react";
 
 
 export default function Navbar() {
     return (
         <Flex width={1160} height={145} left={140} position="absolute" mt="50">
             <Image src="./Vector.svg" alt="vida norturna" w="85" height="85" />
+            <Heading size="1.5rem" mt="90" position="absolute" mx="-1%">vida noturna</Heading>
 
             <Image src="./Praia.svg" ml="13%" alt="praia" w="85" height="85" />
-
-            <Image src="./Moderno.svg" ml="15%" alt="moderno" w="85" height="85" />
+            <Heading size="1.5" mt="90" position="absolute" ml="22%">praia</Heading>
+            
+            <Image src="./Moderno.svg" ml="19%" alt="moderno" w="85" height="85" />
+            <Heading size="1.5" mt="90" position="absolute" ml="46%">moderno</Heading>
 
             <Image src="./museum 1.svg" ml="15%" alt="clássico" w="85" height="85" />
+            <Heading size="1.5" mt="90" position="absolute" ml="67%">clássico</Heading>
 
             <Image src="./earth 1.svg" ml="16%" alt="saiba mais" w="85" height="85" />
+            <Heading size="1.5" mt="90" position="absolute" ml="91%">e mais...</Heading>
 
         </Flex>
 
